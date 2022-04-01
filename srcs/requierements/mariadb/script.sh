83e988656335a97484265031ce0b36e51706c2d2
Mohamed Bifenzi <mbifenzi@e2r11p7.1337.ma>
2022-04-01 22:47:12 +0000

    i think i m done

--- srcs/requierements/mariadb/script.sh
+++ srcs/requierements/mariadb/script.sh
@@ -2,17 +2,17 @@
 # #/etc/init.d/mysql start
 service mysql start
 sleep 5
-mysql -u root -e  "CREATE USER 'mbifenzi'@'localhost' IDENTIFIED BY 'wordpress'; flush privileges;"
 mysql -u root -e  "CREATE DATABASE IF NOT EXISTS wordpress; flush privileges;"
-# mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'localhost' WITH GRANT OPTION IDENTIFIED BY 'wordpress'; flush privileges;"
-mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'localhost' IDENTIFIED BY 'wordpress'; flush privileges;"
-mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'%.%.%.%' IDENTIFIED BY 'wordpress'; flush privileges;"
+mysql -u root -e "use wordpress; grant all privileges on *.* to '${USER}'@'localhost' IDENTIFIED BY '${USER_PASSWORD}'; flush privileges;"
+mysql -u root -e "use wordpress; grant all privileges on *.* to '${USER}'@'%.%.%.%' IDENTIFIED BY '${USER_PASSWORD}'; flush privileges;"
 #---------
-mysql -u root -e  "CREATE USER 'mbifenzi2'@'%.%.%.%' IDENTIFIED BY 'wordpress'; flush privileges;"
-mysql -u root -e "use wordpress; grant all privileges on wordpress.* to 'mbifenzi2'@'%.%.%.%' IDENTIFIED BY 'wordpress2'; flush privileges;"
+mysql -u root -e  "CREATE USER '${SECOND_USER}'@'%.%.%.%' IDENTIFIED BY '${SECOND_PASSWORD}'; flush privileges;"
+mysql -u root -e "use wordpress; grant all privileges on wordpress.* to '${SECOND_USER}'@'%.%.%.%' IDENTIFIED BY '${SECOND_PASSWORD}'; flush privileges;"
 mysql -u root wordpress < wp.sql
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'wordpress'; flush privileges;"
+mysql -u ${USER} -p${USER_PASSWORD} -e "DELETE FROM mysql.user WHERE User='root'; flush privileges;"
 # mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 #/etc/init.d/mysql restart
-service mysql restart 
+# service mysql restart 
 
 sleep infinity
