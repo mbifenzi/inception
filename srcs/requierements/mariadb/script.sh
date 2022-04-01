6200ebddec15182c758dbedabdc54d7d2732d239
Mohamed Bifenzi <mbifenzi@e2r11p7.1337.ma>
2022-04-01 17:37:59 +0000

    ouf

--- srcs/requierements/mariadb/script.sh
+++ srcs/requierements/mariadb/script.sh
@@ -2,11 +2,16 @@
 # #/etc/init.d/mysql start
 service mysql start
 sleep 5
-mysql -u root -e  "CREATE USER 'mbifenzi'@'%.%.%.%' IDENTIFIED BY 'wordpress'; flush privileges;"
-mysql -u root -e  "create database wordpress; flush privileges;"
-# mysql -u root -proot -e "grant all privileges on *.* to 'mbifenzi'@'%.%.%.%' IDENTIFIED by 'wordpress'; flush privileges;"
-mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'%.%.%.%' WITH GRANT OPTION; flush privileges;"
-
+mysql -u root -e  "CREATE USER 'mbifenzi'@'localhost' IDENTIFIED BY 'wordpress'; flush privileges;"
+mysql -u root -e  "CREATE DATABASE IF NOT EXISTS wordpress; flush privileges;"
+# mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'localhost' WITH GRANT OPTION IDENTIFIED BY 'wordpress'; flush privileges;"
+mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'localhost' IDENTIFIED BY 'wordpress'; flush privileges;"
+mysql -u root -e "use wordpress; grant all privileges on *.* to 'mbifenzi'@'%.%.%.%' IDENTIFIED BY 'wordpress'; flush privileges;"
+#---------
+mysql -u root -e  "CREATE USER 'mbifenzi2'@'%.%.%.%' IDENTIFIED BY 'wordpress'; flush privileges;"
+mysql -u root -e "use wordpress; grant all privileges on wordpress.* to 'mbifenzi2'@'%.%.%.%' IDENTIFIED BY 'wordpress2'; flush privileges;"
+mysql -u root wordpress < wp.sql
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 #/etc/init.d/mysql restart
 service mysql restart 
 
